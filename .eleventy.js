3a8f8338a3e63ec6fbb56a7aa56dee0225912a5e
Evan Seils <evanseils2013@gmail.com>
2020-07-12 13:57:53 -0500

    Fix 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -2,11 +2,11 @@ module.exports = function(eleventyConfig) {
   // Output directory: _site
 
   // Copy `img/` to `_site/img`
-  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("src/img");
   
   // Copy `css/fonts/` to `_site/css/fonts`
   // If you use a subdirectory, it’ll copy using the same directory structure.
-  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("src/css");
 
   // Custom Nunjucks Filters
   // Nunjucks Filter
